1d6ab950c7306e36885296a345bf053ca0019bdb
Felipe <Felipe@DESKTOP-1VO2IEC>
2022-02-10 14:58:27 -0300

    Add project files.

---
+++ EXE3.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EXE3.ConsoleApp
+{
+    internal class Program
+    { /* Desenvolver um algoritmo que efetue a soma de todos os números
+ímpares que são múltiplos de três e que se encontram no conjunto dos
+números de 1 até 500.
+       */
+
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 1; i <= 500; i++)
+            {
+                if (i % 2 != 0 && i % 3 == 0)
+                    soma += i;
+            }
+
+            Console.WriteLine("A soma é: " + soma);
+
+            Console.ReadKey();
+        }
+    }
+}
